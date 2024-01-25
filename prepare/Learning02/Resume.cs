38310bff130d77cd2b15dcd50d1dffffa52a694f
Khakichronicles <hesronkkz@icloud.com>
2024-01-24 23:10:16 -0700

    Learning Activity 02 code

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,15 @@
+public class Resume 
+{
+    public string _name;
+
+    public List<Job> _jobs = new List<Job>();
+
+    public void DisplayResume()
+    {
+        System.Console.WriteLine($"{_name}");
+        System.Console.WriteLine("Jobs:");
+        foreach (Job job in _jobs){
+           job.DisplayJob();
+        }
+    }
+}
